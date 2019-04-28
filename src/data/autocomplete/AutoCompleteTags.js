ee5de273c9253503f538f7c62634712ee6153a29
davidholyko <david.holy.ko@gmail.com>
2019-04-28 15:38:00 -0400

    add ruby on rails to autocomplete list

--- src/data/autocomplete/AutoCompleteTags.js
+++ src/data/autocomplete/AutoCompleteTags.js
@@ -1,1 +1,1 @@
-export const autoCompleteTags = 'JavaScript HTML CSS Ruby Java Python Flexbox Algorithms Recursion API MongoDB Express.js React.js PostgreSQL SQL'.split(' ')
+export const autoCompleteTags = 'JavaScript  HTML  CSS  Ruby  Ruby on Rails  Java  Python  Flexbox  Algorithms  Recursion  API  MongoDB  Express.js  React.js  PostgreSQL  SQL'.split('  ')
